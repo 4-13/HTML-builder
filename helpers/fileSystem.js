14b23d029288bfcffb8cbf1e1bfff3f27ff7f62a
4-13 <danilmatvienko@gmail.com>
2023-05-07 22:48:15 +0300

    feat: add few fucnitons to work with fs

--- helpers/fileSystem.js
+++ helpers/fileSystem.js
@@ -1,4 +1,5 @@
-const { readdir, stat } = require('fs/promises');
+const { readdir, stat, mkdir, rm, copyFile } = require('fs/promises');
+const path = require('path');
 
 const getFolderContent = (pathToFolder, options = {}) => {
   return readdir(pathToFolder, { withFileTypes: true, ...options });
@@ -23,6 +24,36 @@ const getFileInfo = async (pathToFIle) => {
   return info;
 };
 
+const createFolder = (path, options = {}) => {
+  return mkdir(path, { recursive: true, ...options });
+};
+
+const removeFolder = (path, options = {}) => {
+  return rm(path, { force: true, recursive: true, ...options });
+};
+
+const copy = (src, dest, mode = 0) => {
+  return copyFile(src, dest, mode);
+};
+
+const copyFolder = async (src, dest) => {
+  const folderContent = await getFolderContent(src);
+  folderContent.forEach((item) => {
+    if (item.isFile()) {
+      copy(path.join(src, item.name), path.join(dest, item.name));
+    } else if (item.isDirectory()) {
+      createFolder(path.join(dest, item.name));
+      copyFolder(path.join(src, item.name), path.join(dest, item.name));
+    }
+  });
+};
+
+
 module.exports.getFolderContent = getFolderContent;
 module.exports.groupFolderContent = groupFolderContent;
 module.exports.getFileInfo = getFileInfo;
+module.exports.copyFile = copyFile;
+module.exports.createFolder = createFolder;
+module.exports.copyFolder = copyFolder;
+module.exports.removeFolder = removeFolder;
+
