d5e6395b04eda9f1a5f355329740b7726f7ca6fd
4-13 <danilmatvienko@gmail.com>
2023-05-07 22:48:22 +0300

    feat: add 04 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const path = require('path');
 
+const { removeFolder, copyFolder, createFolder } = require('../helpers/fileSystem');
+
+const sourceFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
+
+const solution = async () => {
+  await removeFolder(copyFolderPath);
+  await createFolder(copyFolderPath);
+  copyFolder(sourceFolderPath, copyFolderPath);
+};
+
+solution();
