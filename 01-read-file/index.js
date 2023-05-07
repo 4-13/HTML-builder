af7fb473ed404cde5a1454cfac7807f18a808268
4-13 <danilmatvienko@gmail.com>
2023-05-07 03:37:15 +0300

    refactor: rewrite solution 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,16 +1,34 @@
 const path = require('node:path');
 const fs = require('node:fs');
+const { colorSuccessMessage, colorErrorMessage } = require('../helpers/colorString');
 
 const pathToFile = path.resolve(__dirname, './text.txt');
 
-const readFile = (filePath, options, cb) => {
-  const readStream = fs.createReadStream(filePath, options);
+const readFile = async (filePath, options) => {
+  return new Promise((res, rej) => {
+    const readStream = fs.createReadStream(filePath, options);
+    let result = '';
 
-  readStream.on('data', (data) => {
-    cb(data);
-  });
+    readStream.on('data', (data) => {
+      result += data;
+    });
+
+    readStream.on('end', () => {
+      res(result);
+    });
+
+    readStream.on('error', (err) => {
+      rej(err);
+    });
+  }
+  );
 };
 
-readFile(pathToFile, { encoding: 'utf-8' }, (chunk) => {
-  console.log(chunk);
-});
+
+readFile(pathToFile, { encoding: 'utf-8' })
+  .then((data) => console.log(colorSuccessMessage(data)))
+  .catch((err) => console.log(colorErrorMessage(err)));
+
+
+
+
